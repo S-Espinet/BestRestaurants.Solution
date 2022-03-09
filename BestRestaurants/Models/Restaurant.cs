4f62de897e0fcb0cd05118f3cdfb470408a215ff
hmcvay <hayleymcvay8@gmail.com>
2022-03-09 15:18:46 -0800

    Add Cuisine and Restaurant models.
    Co-authored-by: marisa-edgar <marisa-edgar@users.noreply.github.com>
    Co-authored-by: S-Espinet <S-Espinet@users.noreply.github.com>

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Price { get; set; }
+
+    public int CuisineId { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
