b8924210987e12e8411557fcfec5fe5eeff9a1ef
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-10 11:59:58 -0800

    add views and controllers

--- BestRestaurants/Controllers/HomeController.cs
+++ BestRestaurants/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Linq;
+//using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace BestRestaurants.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+    public HomeController(BestRestaurantsContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+  
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Cuisine> model = _db.Cuisines.ToList();
+      return View(model);
+    }
+  }
 }
